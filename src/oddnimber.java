43a8f0a5a337b61cd36e6ea6660d5ac582c724a4
Evgeny <vasilkalin@gmail.com>
2021-04-10 15:29:08 +0200

    Work var

--- src/oddnimber.java
+++ src/oddnimber.java
@@ -1,23 +1,23 @@
 //    Напишите метод printOddNumbers, который принимает массив и выводит в консоль только нечетные числа из него, через запятую. Конец вывода должен перевести курсор на новую строку.
 //    Пример ввода: [3,5,20,8,7,3,100]
 //    Пример вывода: 3,5,7,3
-
+import java.util.Arrays;
+import static java.lang.System.*;
 public class oddnimber {
     public static void main(String[] args) {
-
-        //int[] array = new int[8];
-        int[] array = {3,5,20,8,7,3,100};
-        int[] arraysort = new int[array.length];
-        //Scanner sc = new Scanner(System.in);
-        //a = sc.nextInt();
-        for (int i = 0; i <array.length; i++) {
-            //array[i] = i;
-            if(i / 2 == 0) {
-                arraysort[i] = i;
+        int[] arr = {3, 5, 20, 8, 7, 3, 100};
+        printOddNumbers(arr);
+    }
+    public static void printOddNumbers(int[] arr) {
+        String separator = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print(separator);
+                System.out.print(arr[i]);
+                separator = ",";
             }
         }
-        for(int k= 0; k < arraysort.length; k++) {
-            System.out.print(arraysort[k]);
-        }
+        System.out.println();
+
     }
 }
